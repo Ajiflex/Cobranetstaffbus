9d23477ea46a6164b3144c6f85640e366736945e
Ajiflex <paulajiboye29@gmail.com>
2026-06-08 02:48:15 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use('/api', async (req, res, next) => {
 });
 
 app.use('/api', require('./routes/staffRoutes'));
+app.use('/api/adminBooking', require('./routes/adminBookingRoutes'));
+app.use('/api', require('./routes/busRouteRoutes'));
 app.use('/api', require('./routes/routesreservationRoutes'));
 app.use('/api', require('./routes/routessettingsRoutes'));
 app.use('/api/cron', require('./routes/cronRoutes'));
